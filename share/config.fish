yonas-forks/fish-shell

commit e2b18fc5b63b117a2db2be9d18b110f34f645f80

config.fish: don't load default theme in noninteractive shells
We define colors in noninteractive shells for historical reasons
(because colors used to be universal variables).

The other potential reason is to get regular syntax highlighting for
commands like:

	fish -c 'read --shell'

but if anyone actually uses that they can probably load a theme
explicitly.

Stop defining colors in noninteractive shells.  It's usually not
a good idea to make them behave differently from interactive ones,
but color seems only relevant for interactive shells?

Let's see if anyone complains.. we may end up reverting this if people
want to use noninteractive fish to query colors..  but I'm not sure
why that would be necessary.

Closes #12673

share/config.fish
@@ -206,8 +206,8 @@ end
 
 if status is-interactive
     __fish_migrate
+    fish_config theme choose default --no-override
 end
-fish_config theme choose default --no-override
 
 # As last part of initialization, source the conf directories.
 # Implement precedence (User > Admin > Extra (e.g. vendors) > Fish) by basically doing "basename".

tests/checks/__fish_migrate.fish
@@ -78,6 +78,7 @@ echo no default universal variables
 # Labeled color variables may be updated by fish.
 {
     $fish -c '
+        fish_config theme choose default
         set -g fish_color_autosuggestion red
         set -g fish_color_command green --theme=default
         __fish_migrate

tests/checks/fish_config.fish
@@ -1,6 +1,8 @@
 # RUN: %fish %s
 # REQUIRES: command -v diff
 
+fish_config theme choose default
+
 set -g fish (status fish-path)
 
 fish_config prompt list | string match -r '^(?:acidhub|disco|nim)$'

tests/checks/functions.fish
@@ -299,6 +299,7 @@ functions --no-details --color=never test_color_option
 # CHECK:     echo hello
 # CHECK: end
 
+fish_config theme choose default
 string escape (functions --no-details --color=always test_color_option)
 # CHECK: function\ \e\[36mtest_color_option\e\[32m
 # CHECK: \e\[39m\ \ \ \ echo\ \e\[36mhello\e\[32m

commit b6f30f11e485434d5b5556ec22533dc64263a9e8

webconfig: hack webconfig to fake interactive colors
Commit e2b18fc5b63 (config.fish: don't load default theme in
noninteractive shells, 2026-04-28) broke webconfig: since "fish_config
theme choose default" was removed from non-interactive shells,
webconfig won't know the current theme in interactive shells.

Fix this by adding secret knob that allows webconfig to have
noninteractive fish set the same colors as interactive fish again.

This assumes that plugins won't need the knob, i.e. won't need to
know the "current" theme.

Alternatively, webconfig could run "fish -i" but that could cause
issues if an "if status is-interactive" block in user-config does
something naughty such as writing to stdout even if it's not a terminal.

Alternatively, we could do

	fish -c '
		if test -z "$(__fish_theme_variables)"
			fish_config theme choose default
		end
		# can dump current theme now
	'

but that does not feel as reliable (what if the user explicitly does
"set -e" on all color variables or).

Fixes #12717

share/config.fish
@@ -208,6 +208,8 @@ end
 
 if status is-interactive
     __fish_migrate
+end
+if status is-interactive || set -qgx __fish_force_load_default_theme
     fish_config theme choose default --no-override
 end
 

share/tools/web_config/webconfig.py
@@ -100,13 +100,14 @@ def is_chromeos_garcon():
         return False
 
 
-def run_fish_cmd(text, strict=False):
+def run_fish_cmd(text, strict=False, env=None):
     print("$ " + text)
     p = subprocess.Popen(
         [FISH_BIN_PATH],
         stdin=subprocess.PIPE,
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
+        env=env,
     )
     out, err = p.communicate(text.encode("utf-8"))
     out = out.decode("utf-8", "replace")
@@ -811,7 +812,8 @@ class FishConfigHTTPRequestHandler(SimpleHTTPServer.SimpleHTTPRequestHandler):
                 TERMINAL_COLOR_THEME
             )
             + "or __fish_color_theme=unknown __fish_apply_theme\n"
-            + "__fish_theme_export_for_webconfig"
+            + "__fish_theme_export_for_webconfig",
+            env=os.environ | {"__fish_force_load_default_theme": "1"},
         )
         assert err == ""
 