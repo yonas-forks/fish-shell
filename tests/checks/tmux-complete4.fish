yonas-forks/fish-shell

commit 2f6b1eaaf9ca609a3425000b6c35ce282a26b4a1

reader handle_completions(): don't consider odd replacing completions for common prefix
If "will_replace_token" is set, we generally only consider
appending completions.  This changed in commit 656b39a0b3e (Also show
case-insensitive prefix matches in completion pager, 2025-11-23) which
also allowed icase completions as long as they are also prefix matches.

Such replacing completions might cause the common prefix to be empty,
which breaks the appending completions.

Fix this by not considering these replacing completions for the
common-prefix computation. The pager already doesn't show the prefix
for these completions specifically.

Fixes #12249

src/reader/reader.rs
@@ -6736,6 +6736,9 @@ impl<'a> Reader<'a> {
             let mut prefix_is_partial_completion = false;
             let mut first = true;
             for c in &surviving_completions {
+                if c.flags.contains(CompleteFlags::SUPPRESS_PAGER_PREFIX) {
+                    continue;
+                }
                 if first {
                     // First entry, use the whole string.
                     common_prefix = &c.completion;

tests/checks/tmux-complete2.fish
@@ -36,3 +36,18 @@ tmux-sleep
 isolated-tmux capture-pane -p
 # CHECK: prompt 1> echo $FISH_TEST_VAR_
 # CHECK: $FISH_TEST_VAR_1  (Variable: /)  $FISH_TEST_VAR_2  (Variable: /)
+
+mkdir -p clang/include
+touch clang/INSTALL.txt
+isolated-tmux send-keys C-u ': clang/in' Tab
+tmux-sleep
+isolated-tmux capture-pane -p
+# CHECK: prompt 1> : clang/include/
+# CHECK: clang/INSTALL.txt  …/
+
+mkdir -p clang/include-2
+isolated-tmux send-keys C-u ': clang/in' Tab
+tmux-sleep
+isolated-tmux capture-pane -p
+# CHECK: prompt 1> : clang/include
+# CHECK: clang/INSTALL.txt  …/include/  …/include-2/

commit 1f18b9715f86440a194aed76c470e3f42e32be42

Fix repeated tab causing repeated smartcase completion insertion
Doing

	firefox --pro TAB TAB TAB

results in

	firefox --profile --ProfileManager

git-bisect points to 3546ffa3ef5 (reader
handle_completions(): remove dead filtering code, 2026-01-02)
but that regression has already been fixed by 85e76ba3561
(Fix option substr completions not being filtered out, 2026-04-16).

However in between those two commits, the above case has also been
broken by 2f6b1eaaf9c (reader handle_completions(): don't consider
odd replacing completions for common prefix, 2026-01-02)

The first TAB inserts "--profile ", including the trailing space.
However it also shows the completion pager, which means that subsequent
TABs will insert after the space.

The trailing space does not make sense unless we navigate the pager.
Remove it in all cases, to fix this smartcase scenario.

Alternatively, we could start navigating the pager in this case
(and keep the trailing space), but that's probably too inconsistent.

src/reader/reader.rs
@@ -7091,7 +7091,6 @@ impl<'a> Reader<'a> {
         if all_matches_exact_or_prefix {
             // Try to find a common prefix to insert among the surviving completions.
             let mut flags = CompleteFlags::empty();
-            let mut prefix_is_partial_completion = false;
             let mut first = true;
             for c in &comp {
                 if c.flags.contains(CompleteFlags::SUPPRESS_PAGER_PREFIX) {
@@ -7115,7 +7114,6 @@ impl<'a> Reader<'a> {
 
                     // idx is now the length of the new common prefix.
                     common_prefix = common_prefix.slice_to(idx);
-                    prefix_is_partial_completion = true;
 
                     // Early out if we decide there's no common prefix.
                     if idx == 0 {
@@ -7132,11 +7130,8 @@ impl<'a> Reader<'a> {
             assert!(!use_prefix || !common_prefix.is_empty());
 
             if use_prefix {
-                // We got something. If more than one completion contributed, then it means we have
-                // a prefix; don't insert a space after it.
-                if prefix_is_partial_completion {
-                    flags |= CompleteFlags::NO_SPACE;
-                }
+                // More than one completion contributed, so don't insert a space after it.
+                flags |= CompleteFlags::NO_SPACE;
                 self.completion_insert(
                     common_prefix,
                     token_range.end,

tests/checks/tmux-complete4.fish
@@ -3,9 +3,11 @@
 #REQUIRES: uname -r | grep -qv Microsoft
 
 isolated-tmux-start -C '
+    set -g fish_autosuggestion_enabled 0
     complete : -s c -l clip
     complete : -s q -l qrcode
-    set -g fish_autosuggestion_enabled 0
+    complete true -l profile
+    complete true -l ProfileManager
 '
 touch somefile1
 touch somefile2
@@ -20,11 +22,21 @@ end
 
 tab
 # CHECK: [prompt 0> : -cq]
-
 tab
 # CHECK: [prompt 0> : -cq somefile]
 # CHECK: [somefile1  somefile2]
-
 tab
 # CHECK: [prompt 0> : -cq somefile1]
 # CHECK: [somefile1  somefile2]
+
+isolated-tmux send-keys C-u C-l 'true --pro'
+tab
+# CHECK: [prompt 0> true --profile]
+# CHECK: [--profile  --ProfileManager]
+tab
+# CHECK: [prompt 0> true --profile]
+# CHECK: [--profile  --ProfileManager]
+isolated-tmux send-keys foo
+tmux-sleep
+isolated-tmux capture-pane -p | awk '/./ { print "[" $0 "]" }'
+# CHECK: [prompt 0> true --profile foo]